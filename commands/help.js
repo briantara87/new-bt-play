b8f87049b4e2ab3e16fc4eed196cc0ff7387370f
Glitch (hello-express) <none>
2019-07-28 13:24:27 +0000

    ⛹🏀 Checkpoint ./commands/help.js:3443051/1534:3672600/84 ./watch.json:3443051/2

--- commands/help.js
+++ commands/help.js
@@ -1,33 +1,31 @@
-//This command will be required package discord.js
-const Discord = require("discord.js");
-const { RichEmbed } = require("discord.js");
-const config = require("../config.json");
-const fs = require("fs");
+const Djs = require("discord.js");
 
-exports.run = async(client, msg, args) => { // Modification
-  let crafty = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
-  if(!crafty[msg.guild.id]){ 
-     crafty[msg.guild.id] = {
-       prefix: config.prefix
-     }
+exports.run = async(client, msg, args) => { // Modification. baca dm lg.
+  let opti = args.slice(0).join(' ')
+  let optic = opti.toLocaleLowerCase()
+  if(!opti) return msg.channel.send('help here\n help')
+  
+  if(optic.match('here')) {
+    let embed = new Djs.RichEmbed()
+    .setTitle('Help Command')
+    .setColor('BLUE')
+    .setTimestamp()
+    .setThumbnail(client.user)
+    msg.channel.send(embed)
   }
   
-  //if (!args[0]) {
-    
-  let embed = new Discord.RichEmbed()  // This is a defined for Embed
-  .setColor('RANDOM') // This is a color for The Embed
-  .setTitle(`Beta Commands`) // This for A Title in from Embed
-  .setTimestamp()
-  .setFooter(`Requested By: ${msg.author.tag}`)
-  .addField(":o: Commands:", "Play, Skip, Volume, Prefix, Ping, Stop, Pause, Resume, Loop") // You cant add The Command in Here and You cant add Any more field for The Type Commands
-  msg.channel.send(embed)
-    
-  // This is will be send The Embed
+  if(optic.match('dm')) {
+    let embed = new Djs.RichEmbed()
+    .setTitle('Help Command')
+    .setColor('BLUE')
+    .setTimestamp()
+    .setThumbnail(client.user)
+    msg.author.send(embed)
+  }
 } 
-// Let's test it out!
 
 
-exports.conf = {
+exports.conf = { //Auto fokus anjir. uwau. anying speaker w rusak.z
   aliases: ['h']
 }
 
