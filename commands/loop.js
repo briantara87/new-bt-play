3b6f0b7b076606a79ff22ea7ce9023d98729d46c
Glitch (hello-express) <none>
2019-07-28 12:40:27 +0000

    🍝☕️ Checkpoint ./commands/loop.js:3672600/274 ./watch.json:3672600/2

--- commands/loop.js
+++ commands/loop.js
@@ -1,10 +1,8 @@
 let { RichEmbed } = require('discord.js')
-const {
-  queue
-} = require('../index.js')
 
-exports.run = async(client, msg, args, func, queue, guild, color) => {
-  const serverQueue = queue.get(guild.id);
+exports.run = async(client, msg, args, func, guild, color) => {
+  const queue = new Map()
+ const serverQueue = queue.get(msg.guild.id)
   if (!msg.member.voiceChannel) return msg.channel.send('You are not in a voice channel!');
   if (!serverQueue) return msg.channel.send("This server doesn't have a queue");
   if(serverQueue.voiceChannel.id !== msg.member.voiceChannel.id)  return msg.channel.send(`You must be in **${serverQueue.voiceChannel.name}** to repeat the queue`);
