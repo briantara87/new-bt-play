104a891ded48cc3568ec4784696cf61e1e605f2b
Glitch (hello-express) <none>
2019-07-29 12:16:37 +0000

    👹🙉 Checkpoint ./index.js:5756865/32

--- index.js
+++ index.js
@@ -170,6 +170,7 @@ client.on('message', async msg => {
                 .addField(`Users`, `${client.users.size}`, true)
                 .addField(`Developer`, `Zay#5616`, true)
                 .setFooter("Prefix: z! | This bot is still under Development", "https://images-ext-1.discordapp.net/external/ajWUSzEm6V_PVGGct42J3h8swrQEirySEBKq9J4-5WU/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/604652011857641484/b92a72f9537a38687d19939b056090e5.png")
+                .setTimestamp()
                 .setColor(`RANDOM`);
     msg.channel.send(tagEmbed);
 }
