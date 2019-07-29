60a3a1f5eee2bbb38ef7b8cfd111f07d494d14f4
Glitch (hello-express) <none>
2019-07-29 09:52:25 +0000

    📍🚍 Checkpoint ./commands/help.js:5756865/1257 ./commands/loop.js:5756865/16 ./config.json:5756865/14

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,7 @@
 const Djs = require("discord.js");
 const { RichEmbed, Client } = require("discord.js");
 
+
 //dule.exports.run = async (client, msg, args) => { // Modification. baca dm lg.
 //  let opti = args.slice(0).join(' ')
 //  let optic = opti.toLocaleLowerCase()
@@ -26,13 +27,12 @@ const { RichEmbed, Client } = require("discord.js");
 //
 exports.run = (bot, message, args) => {
         let embed = new RichEmbed()
-        .setTitle(`:ping_pong: Pong!`)
+        .setTitle(`:question: There is 13 Commands`)
         .setColor(`RANDOM`)
-        .addField("Latency", value=
-        .addField("API")
+        .addField("Music", "```play pause loop queue resume skip stop volume reload```",`true`)
+        .addField("Another", "```help ping```", "true")
         message.channel.send(embed);
 }
-
 exports.conf = { //Auto fokus anjir. uwau. anying speaker w rusak.z
   aliases: ['h']
 }
