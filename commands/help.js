476d547a4cd71fffbf23ef777e04e75cec9fea28
Glitch (hello-express) <none>
2019-07-29 10:25:25 +0000

    🌟🌚 Checkpoint ./commands/help.js:5756865/1213

--- commands/help.js
+++ commands/help.js
@@ -26,16 +26,21 @@ const { RichEmbed, Client } = require("discord.js");
 //  }
 //
 exports.run = (bot, message, args) => {
+   let start = Date.now(); message.channel.send(':ping_pong:').then(message => { 
+      message.delete(500);
+        let diff = (Date.now() - start); 
+        let API = Math.round((bot.ping).toFixed())
         let embed = new RichEmbed()
-        .setTitle(`:question: There is 13 Commands`)
+        .setTitle(`:question: There is 9 Commands`)
         .setColor(`RANDOM`)
         .addField("Music", "`play` `pause` `loop` `queue` `resume` `skip` `stop` `volume ` `reload`",`true`)
-        .addField("Another", "`help` `ping`", "true")    
-        .addField("My Prefix is `z!`", "_ _", "true")
+        .addField("Latency", `${diff}ms`, "true") 
+        .addField("API", `${API}ms`, true) 
+        .addField("Prefix", "z!", true)
         .addField("_ _", "_ _", "false")              
-        .setFooter("©Zetsuya Bot | 2019 |Zay#5616")
+        .setFooter("©Zetsuya Bot | 2019 | Zay#5616")
         message.channel.send(embed);
-}
+})}
 exports.conf = { //Auto fokus anjir. uwau. anying speaker w rusak.z
   aliases: ['h']
 }
