e88ad148c4d96a48eddd078fbdd1c54c545d82bf
Glitch (zetsuya) <none>
2019-07-30 11:52:17 +0000

    🍈🚦 Checkpoint ./index.js:5756865/159

--- index.js
+++ index.js
@@ -163,14 +163,12 @@ client.on('message', async msg => {
   if (msg == `<@${client.user.id}>` || msg == `<@!${client.user.id}>`) {
     let tagEmbed = new Discord.RichEmbed()    
     .setAuthor(`Zetsuya | ぜつや`, `https://images-ext-1.discordapp.net/external/ajWUSzEm6V_PVGGct42J3h8swrQEirySEBKq9J4-5WU/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/604652011857641484/b92a72f9537a38687d19939b056090e5.png`)
-    .setcolor(`RANDOM`)
     .addField(`Version`, `0.0.1a`, true)
     .addField(`Node JS`, `9.x`, true)
     .addField(`Library`, `[discord.js](https://discord.js.org/#/)`, true)
-    .addField(`Servers`, `${client.guilds.size}`, true)
-    .addField(`Users`, `${client.users.size}`, true)
     .addField(`Developer`, `Zay#5616`, true)
     .setFooter("©Zetsuya Bot | Prefix: z! | This bot is still under Development")
+    .setColor(`RANDOM`)
     msg.channel.send(tagEmbed);
 }
   
