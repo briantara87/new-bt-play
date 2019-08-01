a83ab269c88140091555c74b93ae6cd4337d8a9c
Glitch (zetsuya) <none>
2019-08-01 10:53:13 +0000

    👍🎿 Checkpoint ./commands/help.js:3672600/2045:5756865/42 ./watch.json:3672600/4 ./commands/cmds.js:3672600/40

--- commands/help.js
+++ commands/help.js
@@ -1,20 +1,28 @@
-const Djs = require("discord.js");
-const { RichEmbed, Client } = require("discord.js");
-const fs = require("fs")
+//This command will be required package discord.js
+const Discord = require("discord.js");
+const { RichEmbed } = require("discord.js");
+const config = require("../config.json");
+const fs = require("fs");
 
-
-
-//
-     
-
-    const args = msg.content.slice(prefix.length).trim().split(' ');
+exports.run = async(client, message, args) => { // Modification
+  let crafty = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
+  if(!crafty[message.guild.id]){ 
+     crafty[message.guild.id] = {
+       prefix: config.prefix
+     }
+  }
+  
+  //if (!args[0]) {
+    //gw solat dulu//ya. udah selese nig.//ndk bisa
+  //dah//thx
+    let embed1 = new RichEmbed()
         let embed = new RichEmbed()
         .setAuthor(`Zetsuya | ぜつや`, `https://images-ext-1.discordapp.net/external/ajWUSzEm6V_PVGGct42J3h8swrQEirySEBKq9J4-5WU/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/604652011857641484/b92a72f9537a38687d19939b056090e5.png`)
         .setColor(`#ecd4fc`)
-        .setThumbnail(bot.user.avatarURL)
+        .setThumbnail(client.user.avatarURL)
         .addField("If you have a question / bug report please report them on", "[Discord](https://discord.gg/Xdcbk2X)\n_ _",`false`)
         .addField("Documentation can be found at","[Github](https://github.com/Zay-Development/Zetsuya-Bot/tree/master)\n_ _", "false")
-        .addField(`Are you don't know how to use this bot? do :`, `${prefix}`, `false`) //Kenapa? ~andra//pen biar prefix per server nya muncul///oh wait
+        .addField(`Are you don't know how to use this bot? do :`, `${crafty[message.guild.id].prefix}cmds`, `false`) //Kenapa? ~andra//pen biar prefix per server nya muncul///oh wait
         .setFooter("©Release 2019 | Zetsuya Bot | This bot is still under Development")
         .setTimestamp()
         message.channel.send(embed);
