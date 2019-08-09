0f9f73ad9947529e6d1d460a96accd5ba34bf595
Glitch (zetsuya) <none>
2019-08-09 09:29:28 +0000

    🚥🗾 Checkpoint ./commands/profile.js:5756865/239

--- commands/profile.js
+++ commands/profile.js
@@ -24,19 +24,19 @@ if (member.user.bot === true) {
                 .setAuthor(`Zetsuya | ぜつや`, `https://images-ext-1.discordapp.net/external/ajWUSzEm6V_PVGGct42J3h8swrQEirySEBKq9J4-5WU/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/604652011857641484/b92a72f9537a38687d19939b056090e5.png`)
                 .setThumbnail((target.displayAvatarURL))
                 .setColor(0xECD4FC)
-                .addField("Full Username", `${member.user.tag}`, inline)
+                .addField("Username", `${member.user.username}`, inline)
+                .addField("Discriminator", `${member.user.discriminator}`, inline)
                 .addField("ID", member.user.id, inline)
                 .addField("Bot", `${bot}`,inline, true)
                 .addField("Status", `${status[member.user.presence.status]}`, inline, true)
                 .addField("Playing", `${member.user.presence.game ? `🎮 ${member.user.presence.game.name}` : "Not playing"}`,inline, true)
                 .addField("Roles", `${member.roles.filter(r => r.id !== message.guild.id).map(roles => `${roles.name}`).join(" **|** ") || "No Roles"}`, true)
-                .addField("Joined Discord At", member.user.createdAt)
+                .addField("Acc Created at", member.user.createdAt.toLocaleString(), true)
                 .setFooter(`Information about ${member.user.username}`)
                 .setTimestamp()
     
             message.channel.send(embed);
 
-            message.delete();
     }
 
     module.exports.help = {
