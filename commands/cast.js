4fc462d7234ac3b915d208facaf77e67471e4538
Glitch (zetsuya) <none>
2019-08-09 11:35:28 +0000

    🐂🍴 Checkpoint ./commands/cast.js:5756865/338 ./commands/cmds.js:5756865/486

--- commands/cast.js
+++ commands/cast.js
@@ -1,12 +1,12 @@
 const Discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
-const member = message.mentions.members.first() || message.guild.members.get(args[0]) || message.member;
+const member = message.member;
   const cmd = args.join(' ').split(' | ')
   
 
   let emb = new Discord.RichEmbed()
-  .addField(`${member.user.username}#${member.user.descriminator} Says`, cmd[0], true)
+  .addField(`${member.user.tag} Says`, cmd[0], true)
   .setColor(`RANDOM`)
   .setTimestamp()
 
