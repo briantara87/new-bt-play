cf44d211294f79cd6ef3d1438da19f3631317252
Glitch (zetsuya) <none>
2019-08-10 09:40:25 +0000

    🚀🎓 Checkpoint ./commands/anime.js:5756865/14 ./commands/report.js:5756865/3109 ./commands/cmds.js:5756865/12

--- commands/cmds.js
+++ commands/cmds.js
@@ -15,8 +15,8 @@ exports.run = (bot, message, args, client) => {
         .setColor(`#ecd4fc`)
         //.addField("Commands :", `:grey_exclamation: HELP\n» If you need help to use this bot\n:grey_question: INFO\n» Information of the bot\n:desktop: CMDS\n» Avaible commands to use\n<:Youtube:588594913243955200> PLAY\n» Play your music with ${crafty[message.guild.id].prefix}play [URL/NAME]\n» Right now our bot is only can play from youtube\n:pause_button: PAUSE\n» You don't want to miss your favorite video / song?\n:stop_button: STOP\n» You don't want Zetsuya tired?\n:repeat: LOOP\n» You want to loop your Video/Songs?\n:speaker: VOLUME\n» Zetsuya is too noisy?\n:next_track: SKIP\n» You don't like it? SKIP IT\n:inbox_tray: INVITE\n» Take Zetsuya with you <3\n:satellite: PING\n» Check the speed that made Zetsuya and you meet`, `false`)
         .addField("Music commands :", "`play  stop  pause  skip  loop  volume  resume  queue`", false)
-        .addField("General commands :", "`cast  profile  help  info  cmds  ping  invite`", false)
-        .addField("Moderation commands :", "`clear  prefix  mute  unmute  kick  warn", false)
+        .addField("General commands :", "`say  profile  help  info  cmds  ping  invite`", false)
+        .addField("Moderation commands :", "`clear  prefix  mute  unmute  kick  warn  ban", false)
         //.addField("Other commands will be added soon :ok_hand:","_ _","true")         //ngapain? ~andra
         .setFooter("©Release 2019 | Zetsuya Bot | This bot is still under Development")
         .setTimestamp()
