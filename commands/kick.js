7b704845a922b67e36912c1366bfe9f5e9202d16
Glitch (zetsuya) <none>
2019-08-10 01:15:07 +0000

    🍓🏓 Checkpoint ./commands/kick.js:5756865/759 ./commands/mute.js:5756865/10 ./commands/cmds.js:5756865/23

--- commands/kick.js
+++ commands/kick.js
@@ -3,15 +3,8 @@ module.exports.run = async (bot, message, args) => {
 
   if (!message.member.hasPermission("KICK_MEMBERS")  && message.author.id !== "291221132256870400") return message.channel.send("Sorry, you don't have permissions to use this!");
     
-  let xdemb = new Discord.RichEmbed()
-  .setColor(`#ecd4fc`)
-  .setTitle("Kick Command")
-  .addField("Description:", `Kick a member`, true)
-  .addField("Usage:", "!kick [user]", true)
-  .addField("Example:" ,"!kick @Odar spam")
-
     let member = message.mentions.members.first();
-    if(!member) return message.channel.send(xdemb)
+    if(!member) return message.channel.send(`Please tag user to kick`)
       
     if(!member.kickable) 
       return message.channel.send("I cannot kick this user!");
--- commands/mute.js
+++ commands/mute.js
@@ -11,12 +11,12 @@ module.exports.run = async (bot, message, args) => {
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Sorry, you don't have permissions to use this!");
   if(tomute.hasPermission("MANAGE_MESSAGES")) return message.channel.send("I cant mute this user");
   if (tomute.id === message.author.id) return message.channel.send("You cannot mute yourself!");
-  let muterole = message.guild.roles.find(`name`, "Odar Mute");
+  let muterole = message.guild.roles.find(`name`, "Mute");
 
   if(!muterole){
     try{
       muterole = await message.guild.createRole({
-        name: "Odar Mute",
+        name: "Mute",
         color: "#000000",
         permissions:[]
       })
