38613f792c261de06b8ca2786545453a3ad36c2d
Glitch (zetsuya) <none>
2020-08-02 03:04:04 +0000

    📚💁 Checkpoint ./commands/help.js:19753995/3105

--- commands/help.js
+++ commands/help.js
@@ -4,34 +4,50 @@ const { RichEmbed } = require("discord.js");
 const config = require("../config.json");
 const fs = require("fs");
 
-exports.run = async(client, message, args) => { // Modification
+exports.run = async (client, message, args) => {
+  // Modification
   let crafty = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
-  if(!crafty[message.guild.id]){ 
-     crafty[message.guild.id] = {
-       prefix: config.prefix
-     }
+  if (!crafty[message.guild.id]) {
+    crafty[message.guild.id] = {
+      prefix: config.prefix
+    };
   }
-  
+
   //if (!args[0]) {
-    //gw solat dulu//ya. udah selese nig.//ndk bisa
+  //gw solat dulu//ya. udah selese nig.//ndk bisa
   //dah//thx
-    let embed1 = new RichEmbed()
-        let embed = new RichEmbed()
-        .setAuthor(`Zetsuya | ぜつや`, `://images-ext-1.discordapp.net/external/a`)
-        .setColor(`#ecd4fc`)
-        .setThumbnail(client.user.avatarURL)
-        .addField("If you have a question / bug report please report them on", "[Discord](https://discord.gg/Xdcbk2X)\n_ _",`false`)
-        .addField("Documentation can be found at","[Github](https://github.com/Zay-Development/Zetsuya-Bot/tree/master)\n_ _", "false")
-        .addField(`Are you don't know how to use this bot? do :`, `${crafty[message.guild.id].prefix}cmds`, `false`) //Kenapa? ~andra//pen biar prefix per server nya muncul///oh wait
-        .setFooter("©Release 2019 | Zetsuya Bot | This bot is still under Development")
-        .setTimestamp()
-        message.channel.send(embed);
+  let embed1 = new RichEmbed();
+  let embed = new RichEmbed()
+    .setAuthor(`BLETIK BT`, `://images-ext-1.discordapp.net/external/a`)
+    .setColor(`#ecd4fc`)
+    .setThumbnail(client.user.avatarURL)
+    .addField(
+      "If you have a question / bug report please report them on",
+      "[Discord](https://discord.gg/quhBQyf)\n_ _",
+      `false`
+    )
+    .addField(
+      "Documentation can be found at",
+      "[Github](https://github.com/Zay-Development/Zetsuya-Bot/tree/master)\n_ _",
+      "false"
+    )
+    .addField(
+      `Are you don't know how to use this bot? do :`,
+      `${crafty[message.guild.id].prefix}cmds`,
+      `false`
+    ) //Kenapa? ~andra//pen biar prefix per server nya muncul///oh wait
+    .setFooter(
+      "©Release 2019 | bletik Bot | This bot is still under Development"
+    )
+    .setTimestamp();
+  message.channel.send(embed);
   message.delete();
-}
-exports.conf = { //Auto fokus anjir. uwau. anying speaker w rusak.z
-  aliases: ['h']
-}
+};
+exports.conf = {
+  //Auto fokus anjir. uwau. anying speaker w rusak.z
+  aliases: ["h"]
+};
 
 exports.help = {
   name: "Help"
-}
+};
