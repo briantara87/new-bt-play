2356739c0a58f83988f06ea55bc828295cecd076
Glitch (btmod-1) <none>
2020-08-02 03:54:41 +0000

    Rewound to commit a13f55fdc5dd7cc2b05480813c094740e07c7049 + 0 edits

--- commands/invite.js
+++ commands/invite.js
@@ -2,11 +2,11 @@ const { RichEmbed, Client, uptime, client } = require("discord.js");
 
 exports.run = (bot, message, args) => {
         let embed = new RichEmbed()
-    .setAuthor(`BLETIK | BT`, `   `BLETIK | BT`,`https://cdn.discordapp.com/avatars/478547279104442368/8d7f341ebda4498869591fbd95e3d807.png?size=256`)
+    .setAuthor(`BLETIK | BT`, `https://images-ext-1.discordapp.net/external/ajWUSzEm6V_PVGGct42J3h8swrQEirySEBKq9J4-5WU/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/604652011857641484/b92a72f9537a38687d19939b056090e5.png`)
     .setThumbnail(bot.user.avatarURL)
-        .addField(`Invite me on your server :snowflake:`, `[Invite link](https://discordapp.com/api/oauth2/authorize?client_id=736930869184299070&permissions=8&scope=bot)`, true)
-    .setFooter("©Release 2019 | bletik Bot | This bot is still under Development")
+        .addField(`Invite me on your server ::`, `[Invite link](https://discordapp.com/api/oauth2/authorize?client_id=604652011857641484&permissions=8&scope=bot)`, true)
+    .setFooter("©Release 2019 | Zetsuya Bot | This bot is still under Development")
     .setTimestamp()
-    .setColor(`#6FA9FF`)
+    .setColor(`#ecd4fc`)
         message.channel.send(embed);
 }
