4fb0b7e47b5cd0a65161b6b413f9ab36fcf99879
Glitch (btmod-1) <none>
2020-08-06 23:51:57 +0000

    👚🏩 Checkpoint ./commands/ping.js:19753995/1236 ./commands/say.js:19753995/282

--- commands/ping.js
+++ commands/ping.js
@@ -1,17 +1,19 @@
 const { RichEmbed, Client } = require("discord.js");
 
 exports.run = (bot, message, args) => {
-   let start = Date.now(); message.channel.send('_ _').then(message => { 
-      message.delete(1);
-        let diff = (Date.now() - start); 
-        let API = Math.round((bot.ping).toFixed())
-        let embed = new RichEmbed()
-        .setTitle(`:ping_pong: Pong!`)
-        .setColor(`#ecd4fc`)
-        .addField("Latency", `${diff}ms`, true)
-        .addField("API", `${API}ms`, true)
-        .setFooter("©Release 2019 | Zetsuya Bot | This bot is still under Development")
-        message.channel.send(embed);
-    });
-}
-
+  let start = Date.now();
+  message.channel.send("_ _").then(message => {
+    message.delete(1);
+    let diff = Date.now() - start;
+    let API = Math.round(bot.ping.toFixed());
+    let embed = new RichEmbed()
+      .setTitle(`:ping_pong: Pong!`)
+      .setColor(`#ecd4fc`)
+      .addField("Latency", `${diff}ms`, true)
+      .addField("API", `${API}ms`, true)
+      .setFooter(
+        "©Release 2019 | bletik Bot | This bot is still under Development"
+      );
+    message.channel.send(embed);
+  });
+};
--- commands/say.js
+++ commands/say.js
@@ -4,11 +4,14 @@ module.exports.run = async (bot, message, args) => {
 const member = message.member;
   const cmd = args.join(' ').split(' | ')
   
+let emb = new Discord.RichEmbed()
 
-  let emb = new Discord.RichEmbed()
-  .addField(`${member.user.tag} Says`, cmd[0], true)
+  .addField, cmd[0], true)
+
   .setColor(`RANDOM`)
+
   .setTimestamp()
+ 
 
   message.channel.send(emb)
   
