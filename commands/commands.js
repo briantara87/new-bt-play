72c0c4626051597212d9f2c9fa2b6e7c8dfc0747
Glitch (btmod-1) <none>
2020-08-07 13:45:17 +0000

    👵✋ Checkpoint ./commands/commands.js:19753995/3574

--- commands/commands.js
+++ commands/commands.js
@@ -1,15 +1,16 @@
 const Discord = require("discord.js");
-
-.setColor(`random`)
-
-    .setThumbnail(client.user.avatarURL)
-
 module.exports.run = async (bot, message, args) => {
-  message.channel.send(
-    "help,8ball,avatar,ban,cat,dog,calc,clear,cmds,donate,info,invite,kick,mute,ping,prefix,profile,report,say,unmute,warn"
-  );
+  let bicon = bot.user.displayAvatarURL;
 
- message.delete();
+  let donateEmb = new Discord.RichEmbed()
+
+    .setColor("random")
+
+    .addfield(
+      "help,8ball,avatar,ban,cat,dog,calc,clear,cmds,donate,info,invite,kick,mute,ping,prefix,profile,report,say,unmute,warn"
+    );
+  message.channel.send(embed);
+  message.delete();
 };
 
 module.exports.help = {
