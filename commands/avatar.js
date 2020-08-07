a401ed79f7b27901f18b711864f5c81da4741bdd
Glitch (btmod-1) <none>
2020-08-07 00:37:58 +0000

    👞🎅 Checkpoint ./commands/ban.js:19753995/11 ./commands/avatar:19753995/3601 ./commands/avatar.js:19753995/44

---
+++ commands/avatar.js
@@ -0,0 +1,236 @@
+const Discord = require("discord.js")
+
+ 
+
+ module.exports.run = async (bot, message, args) => {
+
+module.exports = {
+
+  name: "avatar",
+
+  description: "Shows users avatars",
+
+  aliases: ["profilepicture", "icon", "pfp"],
+
+  usage: "[mention]",
+
+  guildOnly: false,
+
+  enableBots: true,
+
+  secret: false,
+
+  clearCmd: true,
+
+  cooldown: 10,
+
+  roles: [],
+
+  permissions: [],
+
+  execute(message, args) {
+
+    const Discord = require("discord.js");
+
+    let mentionedUser = message.mentions.users.first() || message.author;
+
+    let embed = new Discord.RichEmbed()
+
+      .setImage(mentionedUser.displayAvatarURL)
+
+      .setColor(1056085)
+
+      .setTitle("Avatar")
+
+      .setFooter(
+
+        `bletik`,
+
+        "https://cdn.discordapp.com/avatars/478547279104442368/8d7f341ebda4498869591fbd95e3d807.png?size=256"
+
+      )
+
+      .setTimestamp();
+
+    message.channel.send(embed).then(async embedMessage => {
+
+      await embedMessage.react("🖼");
+
+      await embedMessage.react("ℹ");
+
+      const emoji = {
+
+        PIC_PAGE: "🖼",
+
+        INF_PAGE: "ℹ"
+
+      };
+
+      const collector = new Discord.ReactionCollector(
+
+        embedMessage,
+
+        (reaction, user) =>
+
+          Object.values(emoji).includes(reaction.emoji.name) &&
+
+          !user.bot &&
+
+          mentionedUser,
+
+        {
+
+          time: 180000
+
+        }
+
+      );
+
+      collector.on("collect", (reaction, user) => {
+
+        switch (reaction.emoji.name) {
+
+          case emoji.INF_PAGE: {
+
+            embedMessage.reactions.get(emoji.INF_PAGE).remove(message.author);
+
+            embed.description = "Requested by: " + message.author;
+
+            embed.thumbnail = {
+
+              url: `${mentionedUser.displayAvatarURL}`,
+
+              width: "80",
+
+              height: "80"
+
+            };
+
+            embed.image = null;
+
+            embed.title = "Avatar";
+
+            embed.fields = [
+
+              {
+
+                name: "Target User:",
+
+                value: `${mentionedUser}`,
+
+                inline: true
+
+              },
+
+              {
+
+                name: "Avatar URL link:",
+
+                value: "[Click Me](" + mentionedUser.displayAvatarURL + ")",
+
+                inline: true
+
+              }
+
+            ];
+
+            embedMessage.edit({
+
+              embed
+
+            });
+
+            break;
+
+          }
+
+          case emoji.PIC_PAGE: {
+
+            embedMessage.reactions.get(emoji.PIC_PAGE).remove(message.author);
+
+            embed.description = null;
+
+            embed.fields = null;
+
+            embed.image = {
+
+              url: `${mentionedUser.displayAvatarURL}`,
+
+              width: 300,
+
+              height: 300
+
+            };
+
+            embed.thumbnail = null;
+
+            embedMessage.edit({
+
+              embed
+
+            });
+
+            break;
+
+          }
+
+        }
+
+      });
+
+      collector.on("end", () => {
+
+        embed.description = "Required by: " + message.author;
+
+        embed.thumbnail = {
+
+          url: `${mentionedUser.displayAvatarURL}`,
+
+          width: "80",
+
+          height: "80"
+
+        };
+
+        embed.image = null;
+
+        embed.title = "Avatar";
+
+        embed.fields = [
+
+          {
+
+            name: "Target User:",
+
+            value: `${mentionedUser}`,
+
+            inline: true
+
+          },
+
+          {
+
+            name: "Avatar URL link:",
+
+            value: "[Click Me](" + mentionedUser.displayAvatarURL + ")",
+
+            inline: true
+
+          }
+
+        ];
+
+        embedMessage.edit({
+
+          embed
+
+        });
+
+        embedMessage.clearReactions();
+
+      });
+
+    })
+
+  }
+}}
