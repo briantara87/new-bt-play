4e4fe054bf98f0b9c94824a94bceb580f69a47d2
Glitch (btmod-1) <none>
2020-08-10 02:56:26 +0000

    🛫🏌 Checkpoint ./index.js:19753995/2438

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ const client = new Client({
   fetchAllMember: false
 });
 
+
 const func = require("./functions.js");
 
 const queue = new Collection();
@@ -382,6 +383,8 @@ client.on("message", async msg => {
 
   //End of code CMD Handler
   
+  //when bot ready
+  
 
   // Music Command
 
@@ -568,7 +571,110 @@ function play(guild, song) {
 }
 
 //==================
+const discord = require("discord.js");
 
+const token = require("./token.json").token;
+
+const bot = new discord.Client({
+
+  disableEveryone: true
+
+});
+// When bot ready
+
+bot.on("ready", async () => {
+
+  console.log(`${bot.user.username} is ready for action!`);
+
+  if (config.activity.streaming == true) {
+
+    bot.user.setActivity(config.activity.game, {
+
+      url: 'https://twitch.tv/username'
+
+    });
+
+  } else {
+
+    bot.user.setActivity(config.activity.game, {
+
+      type: 'WATCHING'
+
+    }); //PLAYING, LISTENING, WATCHING
+
+    bot.user.setStatus('dnd'); // dnd, idle, online, invisible
+
+  }
+
+});
+
+// Load commands
+
+bot.commands = new discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+
+  if (err) console.error(err);
+
+  let jsfiles = files.filter(f => f.split(".").pop() === "js");
+
+  if (jsfiles.length <= 0) return console.log("There are no commands to load...");
+
+  console.log(`Loading ${jsfiles.length} commands...`);
+
+  jsfiles.forEach((f, i) => {
+
+    let props = require(`./commands/${f}`);
+
+    console.log(`${i + 1}: ${f} loaded!`);
+
+    bot.commands.set(props.help.name, props);
+
+  });
+
+});
+
+// Message event
+
+bot.afk = new Map();
+
+bot.on("message", async message => {
+
+  if (message.author.bot) return;
+
+  if (message.channel.type === "dm") return;
+
+  let prefix = config.prefix;
+
+  let messageArray = message.content.split(" ");
+
+  let command = messageArray[0].toLowerCase();
+
+  let args = messageArray.slice(1);
+
+  // return message.channel.send(`**${user_tag}** is currently afk. Reason: ${key.reason}`);
+
+  // return message.reply(`you have been removed from the afk list!`).then(msg => msg.delete(5000));
+
+  if (message.content.includes(message.mentions.members.first())) {
+
+    let mentioned = bot.afk.get(message.mentions.users.first().id);
+
+    if (mentioned) message.channel.send(`**${mentioned.usertag}** is currently afk. Reason: ${mentioned.reason}`);
+
+  }
+
+  let afkcheck = bot.afk.get(message.author.id);
+
+  if (afkcheck) return [bot.afk.delete(message.author.id), message.reply(`you have been removed from the afk list!`).then(msg => msg.delete(5000))];
+
+  if (!command.startsWith(prefix)) return;
+
+  let cmd = bot.commands.get(command.slice(prefix.length));
+
+  if (cmd) cmd.run(bot, message, args);
+
+});
 // ============================================================================================================================================
 
 client.login(process.env.TOKEN);
