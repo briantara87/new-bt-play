201baf9105b6cfab084f503770bd07fcdc0963e2
Glitch (btmod-1) <none>
2020-08-10 04:00:36 +0000

    Rewound to commit 80dab4594673818254888169fd1d957e26d90679 + 0 edits

--- index.js
+++ index.js
@@ -92,17 +92,16 @@ function random_playing(bot) {
     `${client.channels.size} Channels`,
     `${client.guilds.size} Server`
   ]; // You cant set anything playing you want it!
-  }
+
   let random = status[Math.floor(Math.random() * status.length)];
 
   client.user.setActivity(random, { type: "WATCHING" });
-
-  //ngk ada error kan?tapi kok ngk muncul//btr//thx
+} //ngk ada error kan?tapi kok ngk muncul//btr//thx
 
 client.on("ready", () => {
   var clientlog = `
 
-[BOT LOGS] bletik Community [BOT LOGS]
+[BOT LOGS] Zetsuya Community [BOT LOGS]
 
 =============================================
 
