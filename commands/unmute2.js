d6bc07897221a621533075abe42bf718f2c8e660
Glitch (btmod-1) <none>
2020-08-10 13:11:26 +0000

    😜🚜 Checkpoint ./commands/mute.js:19753995/6 ./commands/unmute2.js:19753995/2275

--- commands/unmute2.js
+++ commands/unmute2.js
@@ -1,19 +1,29 @@
 module.exports.run = async (bot, message, args) => {
-  if (!message.member.author.id("478547279104442368"))
+
+  if (!message.member.hasPermission( "MANAGE_MESSAGES"))
+
     return message.channel.sendMessage(
-      "You don't have the `bletik` premission"
+
+      "You don't have the `Manage Messages` premission"
+
     );
 
+  
+
   let toMute =
+
     message.guild.member(message.mentions.users.first()) ||
+
     message.guild.members.get(args[0]);
 
   if (!toMute)
+
     return message.channel.sendMessage("Please mention an user or ID to mute!");
 
   let role = message.guild.roles.find(r => r.name === "prey");
 
   if (!role || !toMute.roles.has(role.id))
+
     return message.channel.sendMessage("This user is not muted!");
 
   await toMute.removeRole(role);
@@ -21,8 +31,12 @@ module.exports.run = async (bot, message, args) => {
   message.channel.sendMessage("The user has been unmuted!");
 
   message.delete();
+
 };
 
 module.exports.help = {
+
   name: "unmute"
+
 };
+
