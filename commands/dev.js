3c31c416fdd0453cae1b45de30656aa718114f0b
Glitch (bt-play) <none>
2020-08-12 23:50:36 +0000

    🌓📡 Checkpoint ./commands/dev.js:19753995/155

--- commands/dev.js
+++ commands/dev.js
@@ -2,11 +2,14 @@ const Discord = require("discord.js");
 
 const bot = new Discord.Client();
 
+ 
 exports.run = (client, message, args) => {
 
     const user = message.mentions.users.first();
 
-   
+    if (message.author.id !== "478547279104442368")
+
+ 
 
   	message.guild.member(user).addRole(bletik).then(() => {      message.channel.send("***You have successfully! :white_check_mark:***")
 message.delete ();
