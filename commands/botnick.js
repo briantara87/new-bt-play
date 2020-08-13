3309c20a418c944fc556ea0cbd21f88da26fb575
Glitch (bt-play) <none>
2020-08-13 07:36:50 +0000

    💠😋 Checkpoint ./commands/botnick.js:19753995/4033

---
+++ commands/botnick.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+
+const bot = new Discord.Client();
+
+exports.run = (client, message, args) => {
+  if (!message.member.id("478547279104442368")) {
+    return message.reply(
+      "You do not have permission to change the bot's nickname"
+    );
+  } else {
+    let username = args.join(" ");
+
+    if (username.length < 1)
+      return message.reply("You must supply a name for the client.");
+
+    message.guild.members.get("330044809651814412").setNickname(username);
+
+    const embed = new Discord.RichEmbed()
+
+      .setColor(0x00a2e8)
+
+      .addField(
+        "Bot username set successfully!",
+        username + " is now the nickname for the bot :white_check_mark:"
+      );
+
+    message.reply({ embed });
+  }
+};
+essage.reply({ embed });
+  }
+};
