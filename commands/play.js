edf8a7ed0b236a6b2931214c9a82197b68df8039
Glitch (bt-play) <none>
2020-08-15 11:33:45 +0000

    🚂🔮 Checkpoint ./commands/play.js:19753995/505

---
+++ commands/play.js
@@ -0,0 +1,31 @@
+const musicfuncs = require('discord.js');
+
+module.exports.run = async (bot, message, args, funcs) => {
+
+    try {
+
+        musicfuncs.play(message, args, funcs);
+
+    } catch (e) {
+
+        console.log(e);
+
+        funcs.send(`Oh no! An error occurred! \`${e.message}\`.`);
+
+    }
+
+};
+
+module.exports.config = {
+
+    name: "play",
+
+    aliases: [],
+
+    usage: "Play music with the bot.",
+
+    commandCategory: "music",
+
+    cooldownTime: "10"
+
+  };
