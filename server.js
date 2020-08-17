3435f6702562db78e68ccf79f7c005d148661d2f
Glitch (bt-play--) <none>
2020-08-17 15:26:36 +0000

    🕰🍁 Checkpoint ./server.js:19753995/645

--- server.js
+++ server.js
@@ -1,3 +1,36 @@
+const discord = require("discord.js");
+
+const client = new discord.Client({ disableMentions: "everyone" });
+
+const db = new Map();
+
+const fs = require("fs");
+
+const snek = require("node-superfetch");
+const { prefix } = require("./config.json");
+
+client.aliases = new discord.Collection();
+
+client.commands = new discord.Collection();
+
+client.prefix = prefix;
+
+client.queue = new Map();
+
+client.hastebin = async text => {
+
+  const { body } = await snek
+
+    .post("https://bin-clientdev.glitch.me/documents")
+
+    .send(text);
+
+  return `https://bin-clientdev.glitch.me/${body.key}`;
+
+};
+
+
+
 const http = require('http');
 const express = require('express');
 const app = express();
@@ -10,8 +43,7 @@ setInterval(() => {
   http.get(`http://zetsuya.glitch.me/`);
 }, 280000);
 
-
-
+/*
 
 
 client.on("message", msg => {
@@ -32,5 +64,6 @@ client.on("message", msg => {
 
     msg.channel.send(embed);
 
-  }
-  
+  })
+  client.login(process.env.TOKEN);
+  */
