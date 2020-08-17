2a3bbda39bbd4a458489b6280cd7deba83f9fc97
Glitch (bt-play) <none>
2020-08-17 01:16:02 +0000

    🏣📺 Checkpoint ./index.js:19753995/125 ./server.js:19753995/2240

--- index.js
+++ index.js
@@ -27,25 +27,23 @@ const { loadImage } = require("canvas");
 
 const config = require("./config.json");
 
+require("./server.js");
 
 //const coins = require("./coins.json");
 
 //const xp = require("./xp.json");
-
-const YouTube = require("simple-youtube-api");
+/*const YouTube = require("simple-youtube-api");
 
 const ytdl = require("ytdl-core");
 
 const snekfetch = require("snekfetch");
 
 const cooldown = new Collection();
-
-const ytdlDiscord = require("ytdl-core-discord");
-
+*/
 
 //coconst ytdlDiscord = require("ytdl-core-discord");
 
-const money = require('discord-money');
+//onst money = require('discord-money');
 
 const client = new Client({
   disableEvents: [],
@@ -65,7 +63,7 @@ client.commands = fs.readdirSync("./commands");
 
 client.aliases = {};
 
-const youtube = new YouTube(process.env.YOUTUBE);
+//const youtube = new YouTube(process.env.YOUTUBE);
 
 for (const cmd of client.commands) {
   const file = require(`./commands/${cmd}`);
