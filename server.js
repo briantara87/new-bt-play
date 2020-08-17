06fdee111c45ee64f98ab9f8c469c660293ddacd
Glitch (bt-play--) <none>
2020-08-17 14:53:20 +0000

    👧💡 Checkpoint ./server.js:19753995/366

--- server.js
+++ server.js
@@ -9,3 +9,28 @@ app.listen(process.env.PORT);
 setInterval(() => {
   http.get(`http://zetsuya.glitch.me/`);
 }, 280000);
+
+
+
+
+
+client.on("message", msg => {
+
+  if (msg.author.bot) return;
+
+  if (!msg.guild) return;
+
+  if (msg.content == `<@${client.user.id}>`) {
+
+    const embed = new discord.MessageEmbed()
+
+      .setDescription(`:wave: | My prefix is ${prefix}`)
+
+      .setfooter("to see commands type `87commands`")
+
+      .setColor("RANDOM");
+
+    msg.channel.send(embed);
+
+  }
+  
