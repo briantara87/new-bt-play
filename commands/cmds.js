0971ffe1d345fce9a904203e3adf9c26923c1cd5
Glitch (bt-play--) <none>
2020-08-17 10:37:38 +0000

    🌲💸 Checkpoint ./commands/commands.js:19753995/3259 ./commands/cmds.js:19753995/5059

--- commands/cmds.js
+++ commands/cmds.js
@@ -7,34 +7,31 @@ const config = require("../config.json");
 const fs = require("fs");
 
 exports.run = (bot, message, args, client) => {
-  let crafty = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
 
-  if (!crafty[message.guild.id]) {
-    crafty[message.guild.id] = {
-      prefix: config.prefix
-    };
+    let crafty = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
+
+  if(!crafty[message.guild.id]){ 
+
+     crafty[message.guild.id] = {
+
+       prefix: config.prefix
+
+     }
+
   }
 
-  let embed = new RichEmbed()
+        let embed = new RichEmbed()
 
-    .setAuthor(
-      `Now Playing`,
-      `https://cdn.discordapp.com/attachments/603134109371334666/611156158543036428/music-cd.png`
-    )
+        .setAuthor(`Now Playing`, `https://cdn.discordapp.com/attachments/603134109371334666/611156158543036428/music-cd.png`)
 
-    .setColor(`#ecd4fc`)
+        
+        
+          .setColor(`random`)
+          .set        .setTimestamp()
 
-    .addField(
-      "There is commands you can use",
-
-      `Write \`${crafty[message.guild.id].prefix}(name of the command)\`to use the commands\n\n`,
-
-      false
-    )
-
-    .setTimestamp();
-
-  message.channel.send(embed);
+        message.channel.send(embed);
 
   message.delete();
-};
+
+}
+
--- commands/commands.js
+++ commands/commands.js
@@ -21,22 +21,42 @@ exports.run = (bot, message, args, client) => {
       `Now Playing`,
       `https://cdn.discordapp.com/attachments/603134109371334666/611156158543036428/music-cd.png`
     )
+    .addField(
+      "General commands :",
+
+      "`say  profile  help  info  cmds  ping  invite  avatar commands  donate  serverinfo`",
+
+      false
+    )
+
+    .addField(
+      "Fun commands :",
+
+      "`8ball  age  advice  roast  cat  dog  roll  calc  achievement  fish  poll`",
+
+      false
+    )
+
+    .addField(
+      "Moderation commands :",
+
+      "`clear  prefix  mute  unmute  kick  warn  botnick`",
+
+      false
+    )
     .addfield(
+      "music commands",
       "`loop  nowplaying  pause  play  playlist  queue  remove  remove  search  skip  skipto  stats  stop  volume`",
 
       false
-
     )
 
     .addField("Other commands will be added soon:ok_hand:")
 
     .setFooter(
-
       "©Release 2019 | bletik Bot | This bot is still under Development"
-)
-    
+    )
 
-  
     .setTimestamp();
 
   message.channel.send(embed);
