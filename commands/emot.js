6814a06050673f6b1af77b862b6fd0c892c217fb
Glitch (bt-play--) <none>
2020-08-19 12:31:26 +0000

    🌇🍃 Checkpoint ./commands/emot.js:19753995/496

---
+++ commands/emot.js
@@ -0,0 +1,13 @@
+module.exports.run = async (bot, message, args) => {
+
+   const saymessage = ["<a:s_ngibing:725932668138684446>", ":fish:", ":tropical_fish:", ":blowfish:", ":boot:"];
+  
+  message.delete().catch(O_o => {});
+message.send(${fish})
+};
+
+module.exports.help = {
+
+  name: "emot"
+
+};
