29265e8d8ea466f3cb4406ff65fc7be7c8682820
Glitch (bt-play--) <none>
2020-08-19 11:45:24 +0000

    🛰👆 Checkpoint ./commands/botinfo:19753995/195 ./commands/botinfo.js:19753995/1877

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -4,37 +4,42 @@ const { RichEmbed, Client } = require("discord.js");
 
 const config = require("../config.json");
 
-const fs = require("fs")
-
-;
+const fs = require("fs");
 
 exports.run = (bot, message, args, client) => {
-
   let crafty = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
 
   if (!crafty[message.guild.id]) {
-
     crafty[message.guild.id] = {
-
       prefix: config.prefix
-
     };
-
   }
 
   let embed = new RichEmbed()
 
     .setAuthor(
-
       `bt bot`,
 
       `https://cdn.discordapp.com/attachments/711923492508926006/735016654852849775/artflow_202007211310.png`
-
     )
 
-.addField(
- ${client.users.size} Users`,
+    .addField(
+      `${client.users.size} Users`,
+      )
+    .addfield(
+      `${client.channels.size} Channels`,
+      `false`
+      
+      `${client.guilds.size} Server`
+      `false`
+    )
+    .setFooter(
+      "©Release 2019 | bletik Bot | This bot is still under Development"
+    )
 
-    `${client.channels.size} Channels`,
+    .setTimestamp();
 
-    `${client.guilds.size} Server`
+  message.channel.send(embed);
+
+  message.delete();
+};
