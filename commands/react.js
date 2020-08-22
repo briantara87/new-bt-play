7bd96643c421b2d08ef6d9082bd1b368965a636b
Glitch (bt-play--) <none>
2020-08-22 06:33:26 +0000

    🌽⛰ Checkpoint ./commands/react.js:19753995/179

--- commands/react.js
+++
@@ -1,9 +0,0 @@
-try {
-      var playingMessage = await queue.textChannel.send(`🎶 Started playing: **${song.title}** `);
-      await playingMessage.react("⏭");
-      await playingMessage.react("⏯");
-      await playingMessage.react("🔁");
-      await playingMessage.react("⏹");
-    } catch (error) {
-      console.error(error);
-    }
